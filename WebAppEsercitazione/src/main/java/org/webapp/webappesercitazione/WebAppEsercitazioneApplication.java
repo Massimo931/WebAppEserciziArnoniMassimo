b62f7e96023d52cbf2305457f4a040ceab9c5f3e
Massimo931 <massimoarnoni@gmail.com>
2024-10-28 23:45:32 +0100

    prima esercitazione

---
+++ WebAppEsercitazione/src/main/java/org/webapp/webappesercitazione/WebAppEsercitazioneApplication.java
@@ -0,0 +1,13 @@
+package org.webapp.webappesercitazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppEsercitazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppEsercitazioneApplication.class, args);
+	}
+
+}
---
+++ WebAppEsercitazione/src/test/java/org/webapp/webappesercitazione/WebAppEsercitazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webapp.webappesercitazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppEsercitazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
